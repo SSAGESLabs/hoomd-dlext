df981728923116ed1f3e7f94ea4362742134937d
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2020-09-04 21:11:30 -0500

    Add simple build bash script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#CUDA_PATH=/usr/lib/cuda-10.2/
+HOOMD_ROOT=$(${PYTHON_EXECUTABLE} -c 'import site; print(site.getsitepackages()[0])')/hoomd/
+
+cmake -S . -B build \
+    -DBUILD_TESTING=OFF \
+    -DCOPY_HEADERS=ON \
+    -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} \
+    -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH} \
+    -DHOOMD_ROOT=${HOOMD_ROOT}
+
+cmake --build build --target install -j2
