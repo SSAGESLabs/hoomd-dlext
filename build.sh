ba64e18bcf56e30fa6f55f0f5509832e434196fb
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2020-10-26 22:37:01 -0500

    Fix build script

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #CUDA_PATH=/usr/lib/cuda-10.2/
+PYTHON_EXECUTABLE=$(which python3)
 HOOMD_ROOT=$(${PYTHON_EXECUTABLE} -c 'import site; print(site.getsitepackages()[0])')/hoomd/
 
 cmake -S . -B build \
     -DBUILD_TESTING=OFF \
     -DCOPY_HEADERS=ON \
-    -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} \
-    -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH} \
     -DHOOMD_ROOT=${HOOMD_ROOT}
+    #-DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} \
+    #-DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH} \
 
-cmake --build build --target install -j2
+cmake --build build --target install
