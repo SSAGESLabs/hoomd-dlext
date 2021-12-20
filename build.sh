e2865b281a8810fd89168837ac91e2d3073264e5
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2021-12-19 18:53:04 -0600

    Look for HOOMD first in the current environment

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-#CUDA_PATH=/usr/lib/cuda-10.2/
 PYTHON_EXECUTABLE=$(which python3)
 HOOMD_ROOT=$(${PYTHON_EXECUTABLE} -c 'import site; print(site.getsitepackages()[0])')/hoomd/
 
-cmake -S . -B build \
-    -DBUILD_TESTING=OFF \
-    -DCOPY_HEADERS=ON \
-    -DHOOMD_ROOT=${HOOMD_ROOT}
-    #-DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} \
-    #-DCUDA_TOOLKIT_ROOT_DIR=${CUDA_PATH} \
-
+cmake -S . -B build -DHOOMD_ROOT=${HOOMD_ROOT}
 cmake --build build --target install
