69d746d031ac792d6496d30929bef8ba1be5ba60
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2023-05-16 12:06:04 -0500

    Improve build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PYTHON_EXECUTABLE=$(which python3)
-HOOMD_ROOT=$(${PYTHON_EXECUTABLE} -c 'import site; print(site.getsitepackages()[0])')/hoomd/
+PYTHON_SITELIB=$(${PYTHON_EXECUTABLE} -c 'import sysconfig; print(sysconfig.get_path("purelib"))')
+HOOMD_ROOT="${PYTHON_SITELIB}/hoomd"
 
-cmake -S . -B build -DHOOMD_ROOT="${HOOMD_ROOT}"
+cmake -S . -B build -DCMAKE_FIND_ROOT_PATH="${HOOMD_ROOT}" -DHOOMD_ROOT="${HOOMD_ROOT}"
 cmake --build build --target install
